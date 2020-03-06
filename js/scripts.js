7e08f09023332e22029e5704471f1a803a2faa80
Warren Uhrich <hello@warren.codes>
2020-03-06 11:25:34 -0700

    Add for loop and data-type testing.

--- js/scripts.js
+++ js/scripts.js
@@ -35,3 +35,54 @@ console.log( myVariable );
 /**
  * Other types?
  */
+
+// Array:
+var myArrayOfDataTypes = [
+    // Booleans:
+    true,
+    false,
+    // Null:
+    null,
+    // Number (float/integer):
+    34,
+    168.793,
+    // String:
+    'Hello, World!',
+    // Value not defined:
+    undefined,
+    // Not a number:
+    NaN,
+    // An array in an array!?
+    [1, 2, 3, 4, 5]
+];
+
+/**
+ * Creating elements for a webpage. (Adding to the DOM!)
+ */
+
+ // Create datalist.
+var myDataList = document.createElement( 'DL' ); // Won't show yet! We didn't tell it where to go.
+
+// Let's loop through the array so we don't have to type out each item!
+for ( var i = 0; i < myArrayOfDataTypes.length; i++ )
+{ // ( ASSIGNMENT; TERMINATION CONDITION; ITERATION )
+    // Create datalist title.
+    var dataListTitle = document.createElement( 'DT' );
+    dataListTitle.textContent = typeof myArrayOfDataTypes[i]; // Output the datatype.
+
+    // Create datalist data.
+    var dataListData = document.createElement( 'DD' );
+    dataListData.textContent = myArrayOfDataTypes[i];
+
+    // Add our new elements to our datalist.
+    myDataList.appendChild( dataListTitle );
+    myDataList.appendChild( dataListData );
+}
+
+// Add ALL of that to the body! Yay! We should be able to see it now!
+document.body.appendChild( myDataList );
+
+
+
+
+
