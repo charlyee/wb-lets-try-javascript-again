6847a9f4e6179e6fd5662e5e53af3debe871c68b
Warren Uhrich <hello@warren.codes>
2020-03-10 10:12:45 -0600

    Added more nesting; beware recursive calls!

--- js/day-6.js
+++ js/day-6.js
@@ -22,8 +22,21 @@ function myFunction () {
         console.log( 'Accessing function scope variable in nested function: '+myFunctionScopeVariable ); // We can access variables from a PARENT function. But not the reverse. 
 
         console.log( myVariable ); // Global variables / functions are accessible even all the way in here! NICE!
+
+        // Even more nesting!?
+        function myDoubleNestedFunction () {
+            var doubleNestedVar = 'Double nested var, here!';
+
+            console.log( doubleNestedVar ); // We can access locally, right?
+
+            // myNestedFunction(); // Can I run this here!? Yup! It was declared in/by a parent so we are good to go! Calling a function inside of itself causes recursion --> Unless you have a termination condition this causes endless looping. (My browser is crashing.)
+        }
+
+        myDoubleNestedFunction(); // This works, same scope that the function was defined!
     }
 
+    // myDoubleNestedFunction(); // Can't reach this function here... we're outside of its scope!
+    
     myNestedFunction(); // Let's run and try it.
 
     // console.log( myNestedFunctionVariable ); // Can we see it out here? Nope! The variable is scoped to its origin function, not here...
